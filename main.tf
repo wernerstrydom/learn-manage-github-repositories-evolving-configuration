4b3b7147d8460b1417683c4df244f7185e67fccc
Werner Strydom <bloudraak@users.noreply.github.com>
2024-02-18 12:22:31 -0800

    Initial Configuration

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "github_repository" "this" {
+  for_each           = var.repositories
+  name               = each.value.name
+  description        = each.value.description
+  visibility         = each.value.visibility
+  auto_init          = true
+  archive_on_destroy = true
+  topics             = each.value.topics
+}
+
+resource "github_branch_default" "this" {
+  for_each   = var.repositories
+  repository = github_repository.this[each.key].name
+  branch     = "main"
+}
+
+
---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "organization" {
+  type = string
+  description = "The name of the organization to create the repositories in"
+}
+
+variable "repositories" {
+  type = map(object({
+    name        = string
+    description = string
+    visibility  = string
+    topics      = optional(list(string))
+  }))
+}
